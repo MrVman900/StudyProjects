0dda9f108d2cb0578ba14f25e358aebdbc8d9ee5
MrVman900 <115855903+MrVman900@users.noreply.github.com>
2023-05-04 06:29:33 +0000

    3

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 //go in pizza?
 int main()
 {
+    int x = 5;
+    double res = 0;
     cout << "Hi MrVmen\n";
+    for (int i = 0; i < 5; i += 0,5){
+        res += x;
+    }
+    cout << res << " Привет res и GitHub";
 }
